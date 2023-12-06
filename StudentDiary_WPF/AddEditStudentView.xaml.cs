c82dc819a1e9de606db845feb27dde88c32f0cef
tomwiet <tthanquol@gmail.com>
2023-12-06 13:31:28 +0100

    Add project files.

---
+++ StudentDiary_WPF/AddEditStudentView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace StudentDiary_WPF
+{
+    /// <summary>
+    /// Interaction logic for AddEditStudentView.xaml
+    /// </summary>
+    public partial class AddEditStudentView : MetroWindow
+    {
+        public AddEditStudentView()
+        {
+            InitializeComponent();
+        }
+    }
+}
